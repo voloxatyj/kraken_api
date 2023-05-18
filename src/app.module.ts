62b75c5f4b01f974106e9233f4c64783af065c50
Medynskyy Ostap <medinsky_os@groupbwt.com>
2023-05-18 18:29:53 +0300

    add prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { LoggerMiddleware } from './utils/logger.middleware';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { LoggerMiddleware } from './utils/logger.middleware';
       isGlobal: true,
       envFilePath: '.env',
     }),
+    PrismaModule,
   ],
   controllers: [],
   providers: [],
