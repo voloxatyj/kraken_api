e11401a5d4030e347f6fcd5b780ede1006a1ede0
Medynskyy Ostap <medinsky_os@groupbwt.com>
2023-05-18 15:00:31 +0300

    add logs and swagger in app & remov app un* app.*

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,45 @@
+/* eslint-disable indent */
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Server Instance');
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const configService = app.get(ConfigService);
+  const __prod__ = configService.get('ENVIRONMENT') === 'production';
+
+  /*SWAGGER*/
+  const config = new DocumentBuilder()
+    .addCookieAuth()
+    .setTitle('KRAKEN')
+    .setDescription('The KRAKEN API Documentation')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // Log Env
+  logger.log(`
+                        #######################################
+                           🚀[Environment Mode]: ${configService.get('ENVIRONMENT')}
+                        #######################################
+    `);
+
+  // start server
+  await app.listen(configService.get('PORT'), () => {
+    logger.log(`
+          ############################################################################
+                  🚀[server]: Server is up and running @ ${configService.get(
+                    'PROTOCOL',
+                  )}://${configService.get('HOST')}:${configService.get('PORT')}
+          ############################################################################
+    `);
+  });
 }
 bootstrap();
