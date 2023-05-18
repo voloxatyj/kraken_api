0eb4bd84853d3e83d6ec14667b0a555a10134fee
Medynskyy Ostap <medinsky_os@groupbwt.com>
2023-05-18 18:28:44 +0300

    init prisma and schema

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,29 @@
+import { INestApplication, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  private logger = new Logger(PrismaService.name);
+
+  async onModuleInit() {
+    try {
+      await this.$connect();
+      this.logger.log(`
+               ################################################################
+                  🚀[database]: Create connection to kraken_db successfully
+               ################################################################`);
+    } catch (error) {
+      this.logger.log(error);
+    }
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
