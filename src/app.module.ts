389199fea7aad1d8af729371c95d0f260b8bb101
Medynskyy Ostap <medinsky_os@groupbwt.com>
2023-05-23 11:24:00 +0300

    add existing modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,23 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { ConfigModule } from '@nestjs/config';
 import { LoggerMiddleware } from './utils/logger.middleware';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { CronjobsModule } from './cronjobs/cronjobs.module';
+import { CurrencyModule } from './currency/currency.module';
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
     }),
     PrismaModule,
+    UsersModule,
+    CronjobsModule,
+    CurrencyModule,
   ],
   controllers: [],
   providers: [],
